57bb74e6d8dbfefe13db3fee093b1d275c588ebd
colinbrust <colin.brust@gmail.com>
2023-03-09 15:19:02 -0700

    update setup sql script.

--- setup/setup.sql
+++ setup/setup.sql
@@ -1,18 +1,31 @@
---create schema blm."data";
---drop table "data".counties;
-create table "data".counties();
-alter table "data".counties
-	add column "name" varchar(128),
-	add column county_fips varchar(128),
-	add column model varchar(128),
-	add column scenario varchar(128),
-	add column variable varchar(128),
+create schema blm.future;
+create table future.county();
+alter table future.county
+	add column "name" varchar(64),
+	add column id varchar(64),
+	add column model varchar(64),
+	add column scenario varchar(64),
+	add column variable varchar(64),
 	add column "date" date,
 	add column value numeric;
 
-CREATE UNIQUE INDEX date_first ON counties USING btree (date DESC, county_fips, scenario, variable, model);
-CREATE UNIQUE INDEX variable_first ON counties USING btree (variable, date DESC, county_fips,  scenario, model);
-CREATE UNIQUE INDEX county_first ON counties USING btree (county_fips, variable, date DESC, scenario, model);
+CREATE UNIQUE INDEX county_unique_index ON future.county USING btree (id, variable, date DESC, scenario, model);
 
 set datestyle to ISO, YMD;
-copy "data".counties from '/data/preprocess.csv' delimiter ',' csv header;
+copy future.county (model, scenario, variable, "name", "date", value, id) from '/data/county.csv' delimiter ',' csv header;
+--
+
+create table future.huc();
+alter table future.huc
+	add column "name" varchar(64),
+	add column id varchar(64),
+	add column model varchar(64),
+	add column scenario varchar(64),
+	add column variable varchar(64),
+	add column "date" date,
+	add column value numeric;
+
+CREATE UNIQUE INDEX huc_unique_index ON future.huc USING btree (id, variable, date DESC, scenario, model);
+
+set datestyle to ISO, YMD;
+copy future.huc (model, scenario, variable, "name", "date", value, id) from '/data/huc.csv' delimiter ',' csv header;
