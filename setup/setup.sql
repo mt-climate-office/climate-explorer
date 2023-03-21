b292cce949408aeed8d4a59650dfb7ca3bb336f7
colinbrust <colin.brust@gmail.com>
2023-03-21 12:57:08 -0600

    Add GridMET historical data.

--- setup/setup.sql
+++ setup/setup.sql
@@ -9,10 +9,24 @@ alter table future.county
 	add column "date" date,
 	add column value numeric;
 
-CREATE UNIQUE INDEX county_unique_index ON future.county USING btree (id, variable, date DESC, scenario, model);
+CREATE UNIQUE INDEX county_future_unique_index ON future.county USING btree (id, variable, date DESC, scenario, model);
 
 set datestyle to ISO, YMD;
 copy future.county (model, scenario, variable, "name", "date", value, id) from '/data/county.csv' delimiter ',' csv header;
+
+create schema historical AUTHORIZATION mco;
+create table historical.county();
+alter table historical.county
+	add column "name" varchar(64),
+	add column id varchar(64),
+	add column variable varchar(64),
+	add column "date" date,
+	add column value numeric;
+
+CREATE UNIQUE INDEX county_historical_unique_index ON historical.county USING btree (id, variable, date DESC);
+
+set datestyle to ISO, YMD;
+copy historical.county ("name", id, variable, "date", value) from '/data/county_historical.csv' delimiter ',' csv header;
 
 -- create table future.huc();
 -- alter table future.huc
